6c44a20154698386a7d37501e7e2449ccf0e27d3
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2024-09-13 13:03:03 +0100

    finish adding the trends and follow sidebar

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,12 @@
 import { Prisma } from "@prisma/client";
 
+export const userDataSelect = {
+  id: true,
+  username: true,
+  displayName: true,
+  avatarUrl: true,
+} satisfies Prisma.UserSelect;
+
 export const postDataInclude = {
   user: {
     select: {
