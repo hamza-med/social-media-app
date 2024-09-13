41afc44f22df98e1e397f44ada12a6ba77cb49b7
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2024-09-13 17:39:57 +0100

    add posts skeleton

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -2,6 +2,7 @@
 
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
 import kyInstance from "@/lib/ky";
 import { PostData, PostsPage } from "@/lib/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
@@ -30,10 +31,13 @@ export default function ForYouFeed() {
   //? we have an array inside another array
   const posts = data?.pages.flatMap((page) => page.posts) || [];
 
+  //*Status is pending when the first page is loading
   if (status === "pending") {
-    return <Loader2 className="mx-auto animate-spin" />;
+    return <PostsLoadingSkeleton />;
   }
-
+  if (status === "success" && posts.length === 0 && !hasNextPage) {
+    return <p className="text-center text-muted-foreground">No posts yet</p>;
+  }
   if (status === "error") {
     return (
       <p className="text-center text-destructive">
