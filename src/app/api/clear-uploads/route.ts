fcba5f13dac467184c75f2106aafdefc303a3cb0
Mohamed Hamza <mohamed.hamza@se.linedata.com>
2024-09-17 10:10:36 +0100

    implement cron job to delete orphaned media

---
+++ src/app/api/clear-uploads/route.ts
@@ -0,0 +1,53 @@
+import prisma from "@/lib/prisma";
+import { UTApi } from "uploadthing/server";
+
+//*Vercel will use CRON_SECRET to authenticate automatically
+export async function GET(req: Request) {
+  try {
+    const authHeader = req.headers.get("Authorization");
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return Response.json(
+        { message: "Invalid authorization header" },
+        { status: 401 },
+      );
+    }
+    //*Ensure upload at least 24 hours old
+    const unusedMedia = await prisma.media.findMany({
+      where: {
+        postId: null,
+        ...(process.env.NODE_ENV === "production"
+          ? {
+              createdAt: {
+                lte: new Date(Date.now() - 1000 * 60 * 60 * 24),
+              },
+            }
+          : {}),
+      },
+      select: {
+        id: true,
+        url: true,
+      },
+    });
+    //*Pass the keys to the deleteFiles function which are part of the url
+    new UTApi().deleteFiles(
+      unusedMedia.map(
+        (m) =>
+          m.url.split(`/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`)[1],
+      ),
+    );
+
+    await prisma.media.deleteMany({
+      where: {
+        id: {
+          in: unusedMedia.map((m) => m.id),
+        },
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
